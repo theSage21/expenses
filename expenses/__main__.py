6435123d6ee579bab3016b08c5f637637f975369
arjoonn sharma <arjoonn.94@gmail.com>
2020-10-01 15:44:06 +0530

    cleanup cli

--- expenses/__main__.py
+++ expenses/__main__.py
@@ -1,7 +1,14 @@
 import click
-from expenses.bot import runbot
 
 
 @click.command()
-def run():
+@click.option("--tgtoken", envvar="TG_TOKEN")
+@click.option("--dburl", envvar="DATABASE_URL", default="sqlite:///:memory:")
+def run(tgtoken, dburl):
+    from expenses import const
+
+    const.DATABASE_URL = dburl
+    const.TG_TOKEN = tgtoken
+    from expenses.bot import runbot
+
     runbot()
--- expenses/const.py
+++ expenses/const.py
@@ -1,4 +1,3 @@
-import os
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-TG_TOKEN = os.environ.get("TG_TOKEN")
+# These are set in __main__.py
+DATABASE_URL = None
+TG_TOKEN = None
