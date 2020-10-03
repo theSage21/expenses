0c34154ffd314027b5ee4a6ba7b6d974b2b3cec6
arjoonn sharma <arjoonn.94@gmail.com>
2020-10-03 10:56:56 +0530

    simplify code further

--- expenses/bot.py
+++ expenses/bot.py
@@ -1,6 +1,32 @@
+import re
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from expenses import ops, const
+from . import db
+
+
+# Possible formats in which the amount could be expressed
+AMOUNTS = [
+    re.compile(s)
+    for s in [r".*INR[^\d]*(\S+).*" r".*Rs[^\d]*(\S+).*", r".*RS[^\d]*(\S+).*"]
+]
+
+
+def add_expense(sms):
+    amount = None
+    for rgx in AMOUNTS:
+        amount = rgx.match(sms)
+        if amount:
+            break
+    with db.session() as session:
+        if amount is None:
+            msg = db.Message(sms=sms)
+        else:
+            msg = db.Message(
+                amount=amount.group(1), sms=sms, is_parsed=True, is_expense=False
+            )
+        session.add(msg)
+        session.commit()
 
 
 def record(update, context):
